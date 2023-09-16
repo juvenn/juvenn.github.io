1b640a2202304cd7fdcee5a20b497d70cb9870d2
Juvenn Woo <machese@gmail.com>
2023-09-16 12:23:09 +0800

    Upgrade heroicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,28 @@
 <script>
-  import '../app.css'
-  import '../prism.css'
-  import MoonIcon from 'heroicons-svelte/solid/MoonIcon.svelte'
-  import SunIcon from 'heroicons-svelte/solid/SunIcon.svelte'
-  import { browser } from '$app/environment'
-  import { name } from '$lib/info'
-  import { page } from '$app/stores'
+  import "../app.css";
+  import "../prism.css";
+  import { MoonIcon, SunIcon } from "heroicons-svelte/24/solid";
+  import { browser } from "$app/environment";
+  import { name } from "$lib/info";
+  import { page } from "$app/stores";
 
-  let isDarkMode = browser ? Boolean(document.documentElement.classList.contains('dark')) : true
+  let isDarkMode = browser
+    ? Boolean(document.documentElement.classList.contains("dark"))
+    : true;
 
   function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
+    document.documentElement.classList.add("[&_*]:!transition-none");
     window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
+      document.documentElement.classList.remove("[&_*]:!transition-none");
+    }, 0);
   }
 </script>
 
-<div class="flex flex-col min-h-screen ">
+<div class="flex flex-col min-h-screen">
   <div class="flex flex-col flex-grow w-full px-4 py-2">
-    <header class="flex items-center justify-between w-full max-w-2xl py-4 mx-auto lg:pb-8">
+    <header
+      class="flex items-center justify-between w-full max-w-2xl py-4 mx-auto lg:pb-8"
+    >
       <a
         class="text-lg font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-teal-600 dark:to-teal-400"
         href="/"
@@ -34,15 +37,15 @@
         aria-checked={isDarkMode}
         class="w-5 h-5 sm:h-8 sm:w-8 sm:p-1"
         on:click={() => {
-          isDarkMode = !isDarkMode
-          localStorage.setItem('isDarkMode', isDarkMode.toString())
+          isDarkMode = !isDarkMode;
+          localStorage.setItem("isDarkMode", isDarkMode.toString());
 
-          disableTransitionsTemporarily()
+          disableTransitionsTemporarily();
 
           if (isDarkMode) {
-            document.querySelector('html').classList.add('dark')
+            document.querySelector("html").classList.add("dark");
           } else {
-            document.querySelector('html').classList.remove('dark')
+            document.querySelector("html").classList.remove("dark");
           }
         }}
       >
